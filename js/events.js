951aada486980f1f1c1fa65a5df4a04a3d035fdf
Joan in DC <joanrigdon@gmail.com>
2019-01-17 02:46:23 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 
 
 function pressIt() {
-  $('#typing input').on('keydown', function(key) {
+  $('input').on('keydown', function(key) {
     if (key.which == 71) {
       alert("You pressed 'g'.");
     }
